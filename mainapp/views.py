0a22ca77a1a11ab5655752c1f12047eb1fa5d296
Toras <efpan@mail.ru>
2021-04-27 19:46:00 +0700

    added product list from json file. have a problem with finding it in fixtures, so what done is done.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.templatetags.static import static
-
+import json
+from pathlib import Path
 
 # Create your views here.
 
@@ -13,32 +14,14 @@ def index(request):
 
 
 def products(request):
+    with open(Path(__file__).resolve().parent / "templates/mainapp/fixtures/products.json", "r", encoding='UTF-8') as products_json:
+        products_context = json.load(products_json)
+
+    for product in products_context:
+        product['pic'] = static(product['pic'])
+
     context = {
         'title': 'GeekShop - Каталог',
-        'products': [{'pic': static('vendor/img/products/Adidas-hoodie.png'),
-                      'title': 'Худи черного цвета с монограммами adidas Originals',
-                      'price': '6 090,00 руб.',
-                      'text': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-                     {'pic': static('vendor/img/products/Blue-jacket-The-North-Face.png'),
-                      'title': 'Синяя куртка The North Face',
-                      'price': '23 725,00 руб.',
-                      'text': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-                     {'pic': static('vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'),
-                      'title': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                      'price': '3 390,00 руб.',
-                      'text': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-                     {'pic': static('vendor/img/products/Black-Nike-Heritage-backpack.png'),
-                      'title': 'Черный рюкзак Nike Heritage',
-                      'price': '2 340,00 руб.',
-                      'text': 'Плотная ткань. Легкий материал.'},
-                     {'pic': static('vendor/img/products/Black-Dr-Martens-shoes.png'),
-                      'title': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                      'price': '13 590,00 руб.',
-                      'text': 'Гладкий кожаный верх. Натуральный материал.'},
-                     {'pic': static('vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'),
-                      'title': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                      'price': '2 890,00 руб.',
-                      'text': 'Легкая эластичная ткань сирсакер Фактурная ткань.'}
-                     ]
+        'products': products_context
     }
     return render(request, 'mainapp/products.html', context)
