13917e808d827418bec36b2add4f77ce6648339d
Toras <efpan@mail.ru>
2021-05-03 13:28:02 +0700

    added database dump to fixtures

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.templatetags.static import static
 from mainapp.models import Product, ProductCategory
-import json
 
 # Create your views here.
 
@@ -14,12 +12,6 @@ def index(request):
 
 
 def products(request):
-    with open("mainapp/fixtures/products.json", "r", encoding='UTF-8') as products_json:
-        products_context = json.load(products_json)
-
-    for product in products_context:
-        product['image'] = static(product['image'])
-
     context = {
         'title': 'GeekShop - Каталог',
         'products': Product.objects.all(),
