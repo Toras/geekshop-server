69cfe501c21a1ddf90e410f48d421dec5d25812a
Toras <efpan@mail.ru>
2021-05-03 12:20:09 +0700

    added admin

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,12 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -15,3 +21,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name} | {self.category}'
