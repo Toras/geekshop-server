96d52a646f6c519a873bb6b7da26b370814e36d0
Toras <efpan@mail.ru>
2021-05-03 09:48:50 +0700

    moved fixtures from template.

--- mainapp/views.py
+++ mainapp/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 
 def products(request):
-    with open(Path(__file__).resolve().parent / "templates/mainapp/fixtures/products.json", "r", encoding='UTF-8') as products_json:
+    with open("mainapp/fixtures/products.json", "r", encoding='UTF-8') as products_json:
         products_context = json.load(products_json)
 
     for product in products_context:
