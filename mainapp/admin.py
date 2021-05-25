8591770043512e2179f2b8cdf22a0f9591be86f3
Toras <efpan@mail.ru>
2021-05-25 21:01:15 +0700

    changed Django admin interface for baskets, products, productcategories

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name', 'price', 'quantity')
+    search_fields = ('name',)
+
+
+@admin.register(ProductCategory)
+class ProductCategory(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
