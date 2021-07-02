ebb52c68817bc6a11d0b077903f233aacea1425f
Toras <efpan@mail.ru>
2021-07-02 16:20:31 +0700

    patched vk authentication

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -8,21 +8,23 @@ from authapp.models import UserProfile
 
 
 def save_user_profile(backend, user, response, *args, **kwargs):
-    if backend == 'vk-oauth2':
-        api_url = f"https://api.vk.com/method/users.get?fields=bdate,sex,about&access_token={response['access_token']}"
+    if backend.name == 'vk-oauth2':
+        api_url = f"https://api.vk.com/method/users.get?fields=bdate,sex,about,photo_max_orig&v=5.131&access_token={response['access_token']}"
         vk_response = requests.get(api_url)
         if vk_response.status_code != 200:
             return
         vk_data = vk_response.json()['response'][0]
+        if response['email']:
+            user.email = response['email']
         if vk_data['sex']:
             if vk_data['sex'] == 2:
-                user.userprofile.gender = UserProfile.MAILE
+                user.userprofile.gender = UserProfile.MALE
             elif vk_data['sex'] == 1:
                 user.userprofile.gender = UserProfile.FEMALE
         if vk_data['about']:
             user.userprofile.about_me = vk_data['about']
         if vk_data['bdate']:
-            b_date = datetime.strptime(vk_data['bdate'], '%d.%m%Y').date()
+            b_date = datetime.strptime(vk_data['bdate'], '%d.%m.%Y').date()
             age = timezone.now().date().year - b_date.year
             if age < 18:
                 user.delete()
